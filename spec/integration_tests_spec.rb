2444d648a7a6f64c532ddffe28b2c0b4857d7aee
Tomas <tomasp834@gmail.com>
2019-05-19 18:40:27 -0300

    first integration test using rspec

---
+++ spec/integration_tests_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require_relative '../model/contract'
+require_relative '../model/contract_decorator'
+require_relative '../model/amendment'
+require_relative '../model/mont_amendment'
+require_relative '../model/frequency_amendment'
+require_relative '../model/license'
+require_relative '../exceptions/update_contract_error'
+require_relative '../exceptions/contract_must_have_content_error'
+
+describe 'Integration tests' do
+  ## Ejemplo 1: contrato sin confirmar se puede modificar
+  it 'Can modify a contract without confirmation' do
+    license = License.new(1, 1, %w[volver canal13])
+    contract = Contract.new(client: 'artear', contents: ['Volver al futuro'],
+                            mont: 10_000, license: license)
+    contract.change_mont(200_000)
+    expect(contract.mont).to eq 200_000
+  end
+end
