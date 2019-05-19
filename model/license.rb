d1597f77e77994588caf5c807adbeafcbdf93086
raparicio6 <rodrigoaparicio6@gmail.com>
2019-05-19 02:33:35 -0300

    cheking state of contract when updating license properties

--- model/license.rb
+++ model/license.rb
@@ -1,18 +1,35 @@
 class License
   attr_reader :signals
-  attr_accessor :amount_of_repetitions, :repetition_frequency
+  attr_accessor :amount_of_repetitions, :repetition_frequency, :contract
 
   def initialize(amount_of_repetitions, repetition_frequency, signals)
     @amount_of_repetitions = amount_of_repetitions
     @repetition_frequency = repetition_frequency
     @signals = signals
+    @contract = nil
+  end
+
+  def check_if_can_change_property
+    raise UpdateContractError if @contract.confirmed
   end
 
   def add_signal(signal_name)
+    check_if_can_change_property
     @signals << signal_name
   end
 
   def remove_signal(signal_name)
+    check_if_can_change_property
     @signals.delete(signal_name)
+  end
+
+  def change_amount_of_repetitions(amount_of_repetitions)
+    check_if_can_change_property
+    @amount_of_repetitions = amount_of_repetitions
+  end
+
+  def change_repetition_frequency(repetition_frequency)
+    check_if_can_change_property
+    @repetition_frequency = repetition_frequency
   end
 end
--- spec/license_spec.rb
+++ spec/license_spec.rb
@@ -1,23 +1,48 @@
 require 'rspec'
 require_relative '../model/license'
+require_relative '../model/contract'
 
 describe License do
-  it 'should have properties set' do
-    license = described_class.new(2, 200, ['volver'])
+  let(:contract) { Contract.new }
+  let(:license) { described_class.new(2, 200, ['volver']) }
+
+  it 'should have properties set' do # rubocop:disable RSpec/MultipleExpectations
     expect(license.amount_of_repetitions).to eq(2)
     expect(license.repetition_frequency).to eq(200)
     expect(license.signals).to eq(['volver'])
+    expect(license.contract).to eq(nil)
   end
 
   it 'remove_signal should remove the signal' do
-    license = described_class.new(2, 200, ['volver'])
+    license.contract = contract
     license.remove_signal('volver')
     expect(license.signals.size).to eq(0)
   end
 
   it 'add_signal should add the signal' do
-    license = described_class.new(2, 200, ['volver'])
+    license.contract = contract
     license.add_signal('volver')
     expect(license.signals.size).to eq(2)
+  end
+
+  describe 'change license with confirmated contract' do
+    it 'license with confirmed contract should not be able to change signals' do
+      contract.confirmed = true
+      license.contract = contract
+      expect { license.add_signal('canal 13') }.to raise_error(UpdateContractError)
+      expect { license.remove_signal('volver') }.to raise_error(UpdateContractError)
+    end
+
+    it 'license with confirmed contract should not be able to change amount_of_repetitions' do
+      contract.confirmed = true
+      license.contract = contract
+      expect { license.change_amount_of_repetitions(10) }.to raise_error(UpdateContractError)
+    end
+
+    it 'license with confirmed contract should not be able to change repetition_frequency' do
+      contract.confirmed = true
+      license.contract = contract
+      expect { license.change_repetition_frequency(6000) }.to raise_error(UpdateContractError)
+    end
   end
 end
