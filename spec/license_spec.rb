ec3091bc266132239488d4efeb5927df5139d67b
raparicio6 <rodrigoaparicio6@gmail.com>
2019-05-19 01:58:02 -0300

    changing examples names

--- spec/license_spec.rb
+++ spec/license_spec.rb
@@ -3,21 +3,21 @@ require_relative '../model/license'
 
 describe License do
   it 'should have properties set' do
-    license = described_class.new(2, 200, ['artear'])
+    license = described_class.new(2, 200, ['volver'])
     expect(license.amount_of_repetitions).to eq(2)
     expect(license.repetition_frequency).to eq(200)
-    expect(license.signals).to eq(['artear'])
+    expect(license.signals).to eq(['volver'])
   end
 
   it 'remove_signal should remove the signal' do
-    license = described_class.new(2, 200, ['artear'])
-    license.remove_signal('artear')
+    license = described_class.new(2, 200, ['volver'])
+    license.remove_signal('volver')
     expect(license.signals.size).to eq(0)
   end
 
   it 'add_signal should add the signal' do
-    license = described_class.new(2, 200, ['artear'])
-    license.add_signal('artear')
+    license = described_class.new(2, 200, ['volver'])
+    license.add_signal('volver')
     expect(license.signals.size).to eq(2)
   end
 end
